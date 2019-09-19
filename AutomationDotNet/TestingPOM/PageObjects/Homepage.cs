35c939c7b2e57eea69b36a90f0a9393171d02ab4
Sergiu Soptirean <sergiu.soptirean@hotmail.com>
2019-09-19 11:44:58 +0300

    added configuration manager to project references

--- AutomationDotNet/TestingPOM/PageObjects/Homepage.cs
+++ AutomationDotNet/TestingPOM/PageObjects/Homepage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace TestingPOM.PageObjects
 
         public Homepage GoToHomepage()
         {
-            webdriver.Navigate().GoToUrl(TestData.baseURL);
+            webdriver.Navigate().GoToUrl(System.Configuration.ConfigurationManager.AppSettings["baseURL"]);
 
             return this;
         }
