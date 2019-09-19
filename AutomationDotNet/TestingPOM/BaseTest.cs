7703731f2c15d5f24d7af6de6b90fcba4795e72c
Sergiu Soptirean <sergiu.soptirean@hotmail.com>
2019-09-19 11:32:12 +0300

    created basic stuff for POM framework

---
+++ AutomationDotNet/TestingPOM/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPOM
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TeardownDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ AutomationDotNet/TestingPOM/PageObjects/Homepage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace TestingPOM.PageObjects
+{
+    class Homepage
+    {
+        private IWebDriver webdriver;
+
+        public Homepage(IWebDriver driver)
+        {
+            this.webdriver = driver;
+        }
+        
+        //declaring elements
+        private IWebElement homeHeader => webdriver.FindElement(By.XPath("/html/body/table/tbody/tr/td[1]/center/big/strong"));
+
+        public Homepage GoToHomepage()
+        {
+            webdriver.Navigate().GoToUrl(TestData.baseURL);
+
+            return this;
+        }
+
+        public Homepage CheckHomepageHeader()
+        {
+            Assert.AreEqual("1. Home", homeHeader.Text);
+            Assert.IsTrue(homeHeader.Displayed);
+            return this;
+        }
+    }
+}
